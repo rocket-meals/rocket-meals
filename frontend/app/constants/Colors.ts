52c7e9f93e8679c6863a26ea32fdc1a67d9191b3
Nils <nilsbaumgartner1994@gmail.com>
2024-07-26 13:01:46 +0200

    Adapted Colors for areas

--- frontend/app/constants/Colors.ts
+++ frontend/app/constants/Colors.ts
@@ -3,10 +3,10 @@ const tintColorDark = '#fff';
 
 export class AppAreaColors {
 	static DEFAULT_PROJECT_COLOR: string = "#D14610";
-	static FOODS_COLOR: string | undefined = "#DD0000";
-	static CAMPUS_COLOR: string | undefined = "#00DD00";
-	static HOUSING_COLOR: string | undefined = "#0000DD";
-	static NEWS_COLOR: string | undefined = "#DDDD00";
+	static FOODS_COLOR: string | undefined = "#a9c436"; // https://www.studentenwerk-osnabrueck.de/de/essen.html
+	static CAMPUS_COLOR: string | undefined = undefined;
+	static HOUSING_COLOR: string | undefined = "#1aa378"; // https://www.studentenwerk-osnabrueck.de/de/wohnen.html
+	static NEWS_COLOR: string | undefined = undefined;
 }
 
 export default {
