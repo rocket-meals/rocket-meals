68be4c21d644e0bbf64dfea19f7be9ef32a1b5a1
Nils <nilsbaumgartner1994@gmail.com>
2025-07-28 18:53:25 +0200

    Fix fetch foodOfferInfoItems

--- apps/frontend/app/redux/actions/FoodOfferInfoItems/FoodOfferInfoItems.ts
+++ apps/frontend/app/redux/actions/FoodOfferInfoItems/FoodOfferInfoItems.ts
@@ -4,12 +4,12 @@ import { ServerAPI } from '@/redux/actions/Auth/Auth';
 
 export class FoodOfferInfoItemsHelper extends CollectionHelper<DatabaseTypes.FoodofferInfoItems> {
   constructor(client?: any) {
-    super('foodoffer_info_items', client || ServerAPI.getClient());
+    super('foodoffers_info_items', client || ServerAPI.getClient());
   }
 
   async fetchFoodOfferInfoItems(queryOverride: any = {}) {
     const defaultQuery = {
-      fields: ['*', 'info_item.*', 'info_item.name.*', 'info_item.name.translations.*', 'info_item.image.*'],
+      fields: ['*'],
       limit: -1,
     };
     const query = { ...defaultQuery, ...queryOverride };
